1ae08cb123f4f637d0d448b697236b7b2d133c1a
unknown <jaypee141@gmail.com>
2024-02-29 15:30:13 +0530

    jfhfhf

---
+++ src/App.js
@@ -0,0 +1,77 @@
+// import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [user, setUser] = useState([]);
+  const [start, setStart] = useState(0);
+  const [page, setPage] = useState(1);
+  const [length, setLength] = useState(0);
+
+  useEffect(()=>{
+    const fetchData = async(start) => {
+      try{
+        let response=await axios("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+        // setUser(response.data);
+        setLength(response.data.length);
+        let jp = response.data.slice(start, start+11);
+        setUser(jp);
+        setPage(Math.floor(start/10) +1);
+      }catch(e){
+        console.error()
+      }
+    }
+    fetchData(start);
+  }, [start]);
+
+
+  const handlePrev=(e)=>{
+    if (start >= 10){
+      setStart(start-10);
+
+    }
+  }
+  const handleNext=(e) =>{
+    if (page !== (Math.floor(length/10)+1)){
+      setStart(start +10);
+    }
+
+  }
+
+
+
+
+  return (
+    <div className='App'>
+      <h1>Employee Data Table</h1>
+      <div>
+      <table className='table'>
+        <tr className='heading'>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Action</th>
+        </tr>
+        <tbody className='tbody'>
+          {user.map((item)=>
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.role}</td>
+          </tr>
+          )}
+        </tbody>
+      </table>
+      </div>
+      <div className='button'>
+        <button onClick={handlePrev}>Prev</button>
+        <button>{page}</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+
+    </div>
+  )
+}
+export default App;
